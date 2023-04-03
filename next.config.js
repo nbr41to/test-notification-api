92d9e5b8d72274f1d44c744db31b3079edf5bb81
Knob <59198478+nbr41to@users.noreply.github.com>
2023-04-03 09:57:55 +0900

    sample

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA({
+
+  ...nextConfig,
+});
